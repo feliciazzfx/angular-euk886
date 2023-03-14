6463e0cb0ae47bf7d3c3aaf05df3e8d6a47a1d88
feliciazzfx <125969563+feliciazzfx@users.noreply.github.com>
2023-03-14 16:21:30 +0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,80 +16,24 @@ export class BasicComponent {
           title: '电除尘工艺优化模型',
         },
         {
-          title: 'parent node 12 - folded',
-          items: [
-            {
-              title: 'leaf node 121'
-            },
-            {
-              title: 'leaf node 122'
-            },
-            {
-              title: 'leaf node 123'
-            },
-            {
-              title: 'leaf node 124'
-            }
-          ]
+          title: '湿电除尘工艺优化模型',
         },
-      ]
+      ],
     },
     {
-      title: 'parent node 2 - folded',
+      title: '滤料单元寿命预测',
+      open: true,
       items: [
         {
-          title: 'parent node 21 - expanded',
-          open: true,
-          items: [
-            {
-              title: 'leaf node 211'
-            },
-            {
-              title: 'leaf node 212'
-            },
-            {
-              title: 'leaf node 213'
-            },
-            {
-              title: 'leaf node 214'
-            }
-          ]
+          title: '滤袋',
         },
         {
-          title: 'parent node 22 - folded',
-          items: [
-            {
-              title: 'leaf node 221'
-            },
-            {
-              title: 'leaf node 222'
-            },
-            {
-              title: 'leaf node 223'
-            },
-            {
-              title: 'leaf node 224'
-            }
-          ]
+          title: '滤料',
         },
         {
-          title: 'parent node 23 - folded',
-          items: [
-            {
-              title: 'leaf node 231'
-            },
-            {
-              title: 'leaf node 232'
-            },
-            {
-              title: 'leaf node 233'
-            },
-            {
-              title: 'leaf node 234'
-            }
-          ]
-        }
-      ]
+          title: '滤筒',
+        },
+      ],
     },
   ];
 
